bd14e3ce3b9d508690e40b1de33798df8548026f
Dylan <dbarker1073@gmail.com>
2025-10-08 21:22:40 +0000

    added python code to interact with database

---
+++ 6-menu.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# This example uses a credentials stored in a .env file defining SQL_HOST, SQL_USER, SQL_PWD, and SQL_DB
+
+import mysql.connector, os
+from dotenv import load_dotenv
+load_dotenv()
+
+def getConnection():
+    connection = mysql.connector.connect(
+        host=os.getenv('SQL_HOST'),
+        user=os.getenv('SQL_USER'),
+        password=os.getenv('SQL_PWD'),
+        db=os.getenv('SQL_DB')
+    )
+    return connection
+
+def printTable():
+    connection = getConnection()
+    mycursor = connection.cursor()
+    mycursor.execute("select * from champions")
+    myresult = mycursor.fetchone()
+
+    print("In the champions table, we have the following items: ")
+    while myresult is not None:
+        print(myresult)
+        myresult = mycursor.fetchone()
+    connection.close()
+    print()
+
+def insertIntoTable():
+    champname = input("Please give the name of the champion: ")
+    champorigin = input("Please give the origin of the champion: ")
+    attackstyle = input("Please give the attack style of the champion: ")
+    connection = getConnection()
+    mycursor = connection.cursor()
+    query = "insert into champions (name, origin, attack_style) values (%s, %s,%s);"
+    mycursor.execute(query, (champname, champorigin, attackstyle))
+    connection.commit()
+    connection.close()
+
+def deleteRowFromTable():
+    rowToDelete = input("What is the id of the row to delete? ")
+    connection = getConnection()
+    myCursor = connection.cursor()
+    myCursor.execute("delete from champions where id=%s", (rowToDelete,))
+    connection.commit()
+    connection.close()
+
+def updateRow():
+    rowToUpdate = input("What is the id of the row you want to update? ")
+    connection = getConnection()
+    myCursor = connection.cursor()
+    myCursor.execute("select * from champions where id=%s", (rowToUpdate,))
+    myResult = myCursor.fetchone()
+    print(f"The current row has the value: {myResult}")
+    champname = input("Please give the name of the champion: ")
+    champorigin = input("Please give the origin of the champion: ")
+    attacktype = input("Please give the attack style of the champion: ")
+    myCursor.execute("update champions set name=%s, origin=%s, attack_style=%s where id=%s", (champname, champorigin, attacktype, rowToUpdate))
+    connection.commit()
+    connection.close()
+
+
+menuText = """Please select one of the following options:
+1) Display contents of table
+2) Insert new row to table
+3) Update a row of the table
+4) Delete a row of the table
+q) Quit
+"""
+
+if __name__ == "__main__":
+    menuOption = "1"
+    while menuOption != 'q':
+        menuOption = input(menuText)
+        if menuOption == "1":
+            printTable()
+        elif menuOption == "2":
+            insertIntoTable()
+        elif menuOption == "3":
+            updateRow()
+        elif menuOption == "4":
+            deleteRowFromTable()
